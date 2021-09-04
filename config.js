d42b8b936d8fd418f9192974d749b47cb5baf159
Emre Çağatay KÖSE <emrecgty@users.noreply.github.com>
2021-09-05 01:20:29 +0300

    initial commit

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://aipath.dev',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
+  title: 'Blog by Emre Çağatay Köse',
+  subtitle: 'Pathologist, coder, AI enthusiast',
+  copyright: `${(new Date()).getFullYear()} © All rights reserved.`,
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '',
   useKatex: false,
   menu: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Emre Çağatay Köse',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Pathologist, coder, AI enthusiast',
     contacts: {
       email: '',
       facebook: '#',
